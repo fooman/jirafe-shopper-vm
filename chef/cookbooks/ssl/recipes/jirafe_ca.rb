11f813f75c822ea00cae42059154f829f5b027f6
Michael Schubert <michael@schubert.cx>
2012-07-20 22:01:04 +0000

    We have to import the CA cert into the firefox database to avoid the warnings

--- chef/cookbooks/ssl/recipes/jirafe_ca.rb
+++ chef/cookbooks/ssl/recipes/jirafe_ca.rb
@@ -6,3 +6,20 @@ end
 execute "update CA certificates" do
   command "update-ca-certificates -v"
 end
+
+execute "create firefox profile" do
+  command "export DISPLAY=:0; firefox -CreateProfile jirafe"
+  cwd "/home/vagrant"
+  user "vagrant"
+end
+
+cookbook_file "/tmp/firefox-cert8.db" do
+  source "firefox-cert8.db"
+  mode "0644"
+end
+
+execute "install certificate database into firefox profile" do
+  command "cp -f /tmp/firefox-cert8.db /home/vagrant/.mozilla/firefox/*.jirafe/cert8.db"
+  cwd "/home/vagrant"
+  user "vagrant"
+end
